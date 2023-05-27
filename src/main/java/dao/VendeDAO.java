decde912e4e1ede607d5d332a93dcfb6616a61f4
LorenzoReitani <lorenzo.reitani@mail.polimi.it>
2023-05-27 19:47:59 +0200

    Added some DAO

--- src/main/java/dao/VendeDAO.java
+++ src/main/java/dao/VendeDAO.java
@@ -7,6 +7,8 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 
 public class VendeDAO {
     private Connection con;
@@ -29,10 +31,25 @@ public class VendeDAO {
         }
     }
 
+    public List<Vende> getFiveDiscountedProducts() throws SQLException{
+        List<Vende> prodotti = new ArrayList<Vende>();
+        String query = "SELECT codice_prodotto, codice_forniotre, prezzo, sconto FROM vende v JOIN prodotto p ON p.codice_prodotto = v.codice_prodotto WHERE categoria = 'Informatica' AND sconto > 0 ORDER BY RAND() LIMIT 5";
+        try (PreparedStatement pstatement = con.prepareStatement(query);) {
+            try (ResultSet result = pstatement.executeQuery();) {
+                while (result.next()) {
+                    Vende prodotto = mapRowToVende(result);
+                    prodotti.add(prodotto);
+                }
+            }
+
+        }
+        return prodotti;
+    }
+
     private Vende mapRowToVende(ResultSet result) throws SQLException {
         Vende vende = new Vende();
         vende.setCodiceProdotto(result.getInt("codice_prodotto"));
-        vende.setCodiceFornitore(result.getInt("codice_negozio"));
+        vende.setCodiceFornitore(result.getInt("codice_fronitore"));
         vende.setPrezzo(result.getInt("prezzo"));
         vende.setSconto(result.getFloat("sconto"));
         return vende;
