5bee97a1c194a5e0a2b8a128658865f6be83366f
LorenzoReitani <lorenzo.reitani@mail.polimi.it>
2023-07-05 19:30:05 +0200

    migliorato alcuni file html e creato la servlet CreaOrdiniServlet

---
+++ src/main/java/controllers/CreaOrdineServlet.java
@@ -0,0 +1,16 @@
+package controllers;
+
+import javax.servlet.annotation.WebServlet;
+
+@WebServlet("/CreaOrdine")
+public class CreaOrdineServlet extends ServletPadre{
+
+    public CreaOrdineServlet() {
+        super();
+    }
+
+    public void doPost(){
+
+    }
+
+}
