5511023a69f375c32bb923cf2fd75c9b9b78a857
LorenzoReitani <lorenzo.reitani@mail.polimi.it>
2023-07-08 23:16:22 +0200

    reso il server coerente

--- dbNew.sql
+++ dbNew.sql
@@ -27,7 +27,7 @@ CREATE TABLE IF NOT EXISTS tiw.ordini (
     nome_fornitore VARCHAR(255),
     data_spedizione DATE NOT NULL,
     indirizzo_spedizione VARCHAR(255) NOT NULL,
-    prezzo_totale INT NOT NULL CHECK (prezzo_totale > 0),
+    prezzo_totale FLOAT NOT NULL CHECK (prezzo_totale > 0),
     email VARCHAR(255),
     FOREIGN KEY (email) REFERENCES utente(email)
 );
@@ -64,7 +64,7 @@ create table IF NOT EXISTS tiw.visualizza(
 create table IF NOT EXISTS tiw.vende(
     codice_fornitore int not null,
     codice_prodotto int not null,
-    prezzo int not null check (prezzo > 0),
+    prezzo float not null check (prezzo > 0),
     sconto float not null check (sconto >= 0 and sconto < 1),
     primary key (codice_fornitore, codice_prodotto),
     foreign key (codice_fornitore) references fornitore(codice_fornitore),
@@ -77,7 +77,7 @@ create table IF NOT EXISTS tiw.informazioni(
     nome VARCHAR(255) not null,
     foto VARCHAR(255) not null,
     quantita int not null check (quantita > 0),
-    prezzo_unitario int not null check (prezzo_unitario > 0),
+    prezzo_unitario float not null check (prezzo_unitario > 0),
     primary key (codice_ordine, codice_prodotto),
     foreign key (codice_ordine) references ordini(codice_ordine)
 );
@@ -133,9 +133,9 @@ VALUES (1, 1, 10, 30),
 
 -- Popolazione della tabella "ordini"
 INSERT INTO tiw.ordini (nome_fornitore, data_spedizione,indirizzo_spedizione, prezzo_totale, email)
-VALUES ('Fornitore A', '2023-05-20','Via Roma 1', 440, 'utente1@example.com'),
-       ('Fornitore B', '2023-05-21','Via Milano 2', 360, 'utente2@example.com'),
-       ('Fornitore B', '2023-05-22','Via Napoli 3', 3499, 'utente3@example.com');
+VALUES ('Fornitore A', '2023-05-20','Via Roma 1', 1587.5, 'utente1@example.com'),
+       ('Fornitore B', '2023-05-21','Via Milano 2', 16098, 'utente2@example.com'),
+       ('Fornitore B', '2023-05-22','Via Napoli 3', 875, 'utente3@example.com');
 
 -- Popolazione della tabella "visualizza"
 INSERT INTO tiw.visualizza (email, codice_prodotto, data)
@@ -152,15 +152,16 @@ VALUES ('utente1@example.com', 1, 20090521153614),
 -- Popolazione della tabella "vende"
 INSERT INTO tiw.vende (codice_fornitore, codice_prodotto, prezzo, sconto)
 VALUES (1, 1, 100, 0.1),
-       (1, 2, 80, 0.05),
+       (1, 2, 1450, 0.05),
        (1,4,15000,0),
        (1,7,620,0),
        (1,10,180,0),
        (1,12,30,0),
        (1,15,45,0),
        (1,17,78,0),
-       (2, 2, 70, 0.15),
+       (2, 2, 1670, 0.15),
        (2, 3, 60, 0.2),
+       (2,4,16000,0),
        (2,6,230,0),
        (2,8,650,0.2),
        (2,10,210,0),
@@ -179,11 +180,11 @@ VALUES (1, 1, 100, 0.1),
 -- Popolazione della tabella "informazioni"
 INSERT INTO tiw.informazioni (codice_ordine, codice_prodotto, nome, foto, quantita, prezzo_unitario)
 VALUES
-    (1, 1, 'Smartphone Galaxy S21', 'smartphone_galaxy_s21.jpg', 2, 899),
-    (1, 2, 'Laptop ThinkPad X1 Carbon', 'laptop_thinkpad_x1_carbon.jpg', 1, 1599),
-    (2, 3, 'Scarpe da running Nike Air Zoom Pegasus 38', 'scarpe_running_nike_pegasus_38.jpg', 1, 129),
-    (2, 4, 'Orologio automatico Rolex Submariner', 'orologio_rolex_submariner.jpg', 1, 12000),
-    (3, 5, 'Macchina fotografica Canon EOS R5', 'macchina_fotografica_canon_eos_r5.jpg', 1, 3499);
+    (1, 1, 'Smartphone Galaxy S21', 'smartphone_galaxy_s21.jpg', 2, 90),
+    (1, 2, 'Laptop ThinkPad X1 Carbon', 'laptop_thinkpad_x1_carbon.jpg', 1, 1377.5),
+    (2, 3, 'Scarpe da running Nike Air Zoom Pegasus 38', 'scarpe_running_nike_pegasus_38.jpg', 1, 48),
+    (2, 4, 'Orologio automatico Rolex Submariner', 'orologio_rolex_submariner.jpg', 1, 16000),
+    (3, 5, 'Macchina fotografica Canon EOS R5', 'macchina_fotografica_canon_eos_r5.jpg', 1, 850);
 
 
 
