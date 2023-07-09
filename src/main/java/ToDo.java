ec1a8bb45cea927af3752d33be2cd91e01c31fef
LorenzoReitani <lorenzo.reitani@mail.polimi.it>
2023-07-09 15:35:26 +0200

    nuova to do list

---
+++ src/main/java/ToDo.java
@@ -0,0 +1,7 @@
+//TODO sistemare i prodotti consigliati nella pagina home
+
+//TODO mettere la soglia di quantità per la spedizione gratuita in base al prezzo e non alla quantità di prodotti
+
+//TODO verificare se la barra di ricerca e i prodotti consigliati devono apparire in ogni pagina o solo nella home
+
+//TODO ordinare secondo il prezzo i risultati della ricerca
