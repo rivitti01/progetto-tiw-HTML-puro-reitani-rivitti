ce9857e6e5a08e7b2d5304cf124609619388f156
LorenzoReitani <lorenzo.reitani@mail.polimi.it>
2023-07-16 13:50:38 +0200

    database aggiornato

--- dbNew.sql
+++ dbNew.sql
@@ -30,7 +30,7 @@ CREATE TABLE IF NOT EXISTS tiw.ordini (
     indirizzo_spedizione VARCHAR(255) NOT NULL,
     prezzo_totale FLOAT NOT NULL CHECK (prezzo_totale > 0),
     email VARCHAR(255),
-    FOREIGN KEY (email) REFERENCES utente(email)
+    FOREIGN KEY (email) REFERENCES utente(email) ON DELETE NO ACTION ON UPDATE CASCADE
 );
 CREATE TABLE IF NOT EXISTS tiw.fasce(
     codice_fornitore int not null,
@@ -38,21 +38,7 @@ CREATE TABLE IF NOT EXISTS tiw.fasce(
     max int not null check (max > 0),
     prezzo int not null check (prezzo > 0),
     primary key (codice_fornitore, min),
-    foreign key (codice_fornitore) references fornitore(codice_fornitore)
-
-
-    /*,CONSTRAINT check_intervalli_unici CHECK (NOT EXISTS (
-        SELECT 1 FROM fasce AS f2
-        WHERE f2.codice_fornitore = fasce.codice_fornitore
-        AND f2.min <> fasce.min
-        AND ((f2.min <= fasce.min AND f2.max >= fasce.min) OR (f2.min <= fasce.max AND f2.max >= fasce.max)
-                OR (f2.min >= fasce.min AND f2.max <= fasce.max))
-    )),
-    CONSTRAINT check_zero_spazi CHECK (EXISTS (
-        SELECT 1 FROM fasce AS f2
-        WHERE f2.codice_fornitore = fasce.codice_fornitore
-        AND (f2.min = fasce.max + 1 OR f2.min = 1)
-    ))*/
+    foreign key (codice_fornitore) references fornitore(codice_fornitore) on delete cascade on update cascade
 );
 create table IF NOT EXISTS tiw.visualizza(
     email VARCHAR(255),
@@ -60,7 +46,7 @@ create table IF NOT EXISTS tiw.visualizza(
     data timestamp default current_timestamp,
     primary key (email, codice_prodotto),
     foreign key (email) references utente(email),
-    foreign key (codice_prodotto) references prodotto(codice_prodotto)
+    foreign key (codice_prodotto) references prodotto(codice_prodotto) on delete cascade on update cascade
 );
 create table IF NOT EXISTS tiw.vende(
     codice_fornitore int not null,
@@ -68,8 +54,8 @@ create table IF NOT EXISTS tiw.vende(
     prezzo float not null check (prezzo > 0),
     sconto float not null check (sconto >= 0 and sconto < 1),
     primary key (codice_fornitore, codice_prodotto),
-    foreign key (codice_fornitore) references fornitore(codice_fornitore),
-    foreign key (codice_prodotto) references prodotto(codice_prodotto)
+    foreign key (codice_fornitore) references fornitore(codice_fornitore) on delete cascade on update cascade,
+    foreign key (codice_prodotto) references prodotto(codice_prodotto) on delete cascade on update cascade
 );
 
 create table IF NOT EXISTS tiw.informazioni(
@@ -80,7 +66,7 @@ create table IF NOT EXISTS tiw.informazioni(
     quantita int not null check (quantita > 0),
     prezzo_unitario float not null check (prezzo_unitario > 0),
     primary key (codice_ordine, codice_prodotto),
-    foreign key (codice_ordine) references ordini(codice_ordine)
+    foreign key (codice_ordine) references ordini(codice_ordine) on delete cascade on update cascade
 );
 
 
